7872921d9f1e4cd290a316ef5168476335d5bead
Tam Sayers <tamsayers@hotmail.com>
2015-02-21 20:27:43 +0000

    refactored acceptance test to load test data from file

--- test/acceptance/ExpensesForDatesAccTest.scala
+++ test/acceptance/ExpensesForDatesAccTest.scala
@@ -7,30 +7,26 @@ import org.scalatest.GivenWhenThen
 import org.scalatestplus.play.OneAppPerSuite
 import play.api.test._
 import play.api.test.Helpers._
-
 import models.expenses.TestHelpers._
 import java.time.LocalDate
 import play.api.libs.json.Json
+import scala.io.Source
 
-class AddExpensesAccTest extends PlaySpec with OneAppPerSuite with RouteInvokers with Writeables {
+class ExpensesForDatesAccTest extends PlaySpec with OneAppPerSuite with RouteInvokers with Writeables {
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  "posted expenses data" should {
-    val expenses = Seq(
-      testExpense(date = LocalDate.of(2014,12,12)),
-      testExpense(date = LocalDate.of(2015,1,1)),
-      testExpense(date = LocalDate.of(2015,1,19)),
-      testExpense(date = LocalDate.of(2015,2,1))
-    )
+  val toAddJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/toAdd.json")).mkString
+  val expectedJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/expected.json")).mkString
 
+  "posted expenses data" should {
     "be saved for retrieval" in {
-      val result = route(FakeRequest("POST", "/expenses").withBody(Json.toJson(expenses))).flatMap { result =>
+      val result = route(FakeRequest("POST", "/expenses").withBody(Json.parse(toAddJson))).flatMap { result =>
         Thread.sleep(1000)
         route(FakeRequest("GET", "/expenses?from=2015-01-01&till=2015-01-31"))
       }
 
       result mustBe 'defined
-      contentAsString(result.get) mustBe Json.toJson(expenses.slice(1, 3)).toString
+      contentAsString(result.get) mustBe Json.parse(expectedJson).toString
     }
   }
 }
