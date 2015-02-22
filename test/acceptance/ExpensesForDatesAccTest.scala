2007070a2b40412385d5002a245a53f57f080f3b
Tam Sayers <tamsayers@hotmail.com>
2015-02-22 22:20:24 +0000

    added acceptance test for supplier date range selection

--- test/acceptance/ExpensesForDatesAccTest.scala
+++ test/acceptance/ExpensesForDatesAccTest.scala
@@ -11,15 +11,22 @@ import models.expenses.TestHelpers._
 import java.time.LocalDate
 import play.api.libs.json.Json
 import scala.io.Source
+import org.scalatest.BeforeAndAfterAll
 
-class ExpensesForDatesAccTest extends PlaySpec with OneAppPerSuite with RouteInvokers with Writeables {
+class ExpensesForDatesAccTest extends PlaySpec
+    with OneAppPerSuite
+    with RouteInvokers
+    with Writeables
+    with BeforeAndAfterAll {
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
   val toAddJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/toAdd.json")).mkString
+  val forSupplierJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/supplier.json")).mkString
   val expectedJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/expected.json")).mkString
+  val expectedForSupplierJson = Source.fromURL(getClass.getResource("/resources/acceptance/daterange/expectedForSupplier.json")).mkString
 
-  "posted expenses data" should {
-    "be saved for retrieval" in {
+  "expenses for a date range" should {
+    "be retrieved" in {
       val result = route(FakeRequest("POST", "/expenses").withBody(Json.parse(toAddJson))).flatMap { result =>
         Thread.sleep(1000)
         route(FakeRequest("GET", "/expenses?from=2015-01-01&till=2015-01-31"))
@@ -27,6 +34,16 @@ class ExpensesForDatesAccTest extends PlaySpec with OneAppPerSuite with RouteInv
 
       result mustBe 'defined
       contentAsString(result.get) mustBe Json.parse(expectedJson).toString
+    }
+
+    "be retrieved for a specific supplier" ignore {
+      val result = route(FakeRequest("POST", "/expenses").withBody(Json.parse(forSupplierJson))).flatMap { result =>
+        Thread.sleep(1000)
+        route(FakeRequest("GET", "/expenses?from=2015-02-01&till=2015-02-15&supplier=test-supplier"))
+      }
+
+      result mustBe 'defined
+      contentAsString(result.get) mustBe Json.parse(expectedForSupplierJson).toString
     }
   }
 }
