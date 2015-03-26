081b09ba15927a1e1828312a7496b16d30d9e83a
Tam Sayers <tamsayers@hotmail.com>
2015-03-26 22:47:58 +0000

    updated cors filter to add headers for all requests

--- app/filters/CorsFilter.scala
+++ app/filters/CorsFilter.scala
@@ -12,13 +12,16 @@ import scala.async.Async._
 object CorsFilter extends Filter {
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = rh.method match {
+  def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
+    def wrap = rh.method match {
     case "OPTIONS" => async(Results.NoContent)
-    case _ => f(rh).map {
-      _.withHeaders(
-        "Access-Control-Allow-Origin" -> "*",
-        "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers" -> "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept")
+    case _ => f(rh)
+  }
+    wrap.map {_.withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept")
     }
+
   }
 }
--- test/filters/CorsFilterSpec.scala
+++ test/filters/CorsFilterSpec.scala
@@ -9,41 +9,42 @@ import scala.async.Async._
 import play.api.mvc.Result
 import play.api.mvc.RequestHeader
 import scala.concurrent.Future
-import org.mockito.Mockito
+import org.mockito.Mockito._
 import play.api.test.DefaultAwaitTimeout
 import play.api.test.FutureAwaits
 import play.api.mvc.Results
 
 class CorsFilterSpec extends PlaySpec
-    with MockitoSugar
-    with FutureAwaits
-    with DefaultAwaitTimeout {
+  with MockitoSugar
+  with FutureAwaits
+  with DefaultAwaitTimeout {
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  "the cors filter" should {
-    "return 204 for OPTIONS request" in {
-      val requestHeader = mock[RequestHeader]
-      Mockito.when(requestHeader.method).thenReturn("OPTIONS")
+  trait testFilter {
+    val requestHeader = mock[RequestHeader]
 
-      await(CorsFilter(null)(requestHeader)) mustBe Results.NoContent
+    def verifyHeadersFor(expectedResult: Result, toResult: RequestHeader => Future[Result]) = {
+      await(CorsFilter(toResult)(requestHeader)) mustBe expectedResult.withHeaders(
+        "Access-Control-Allow-Origin" -> "*",
+        "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers" -> "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept")
+    }
+  }
+
+  "the cors filter" should {
+    "return 204 for OPTIONS request" in new testFilter {
+      when(requestHeader.method).thenReturn("OPTIONS")
+
+      verifyHeadersFor(Results.NoContent, null)
     }
 
-    "add the cross domain header" in {
-      val result = mock[Result]
-      val resultWithHeaders = mock[Result]
-      val requestHeader = mock[RequestHeader]
-
-      Mockito.when(result.withHeaders(
-          "Access-Control-Allow-Origin" -> "*",
-          "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers" -> "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept")
-      ).thenReturn(resultWithHeaders)
-
+    "add the cross domain header to requests" in new testFilter {
+      val result = Results.Ok
       val toResult: RequestHeader => Future[Result] = { rh =>
         if (rh == requestHeader) async(result) else fail()
       }
 
-      await(CorsFilter(toResult)(requestHeader)) mustBe resultWithHeaders
+      verifyHeadersFor(result, toResult)
     }
   }
 }
