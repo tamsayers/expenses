eca46b07bba5577d559e81c38ef9321440d0eea5
Tam Sayers <tamsayers@hotmail.com>
2015-12-10 20:28:58 +0000

    added more tests

--- app/services/AuthenticationService.scala
+++ app/services/AuthenticationService.scala
@@ -2,6 +2,7 @@ package services
 
 import scala.concurrent.Future
 import models.auth.Authentication
+import models.user._
 import play.api.libs.Crypto
 import models.auth.Authenticated
 import models.auth.Unauthenticated
@@ -14,10 +15,11 @@ trait AuthenticationService {
 class CryptoAuthenticationService(crypto: Crypto, userService: UserService)
                                  (implicit val ex: ExecutionContext) extends AuthenticationService {
   def authenticate(userName: String, password: String): Future[Authentication] = {
+    def isValid(user: User): Boolean = crypto.sign(password, user.secretKey.getBytes).equals(user.passwordHash)
+    
     userService.forName(userName).map { user =>
       user match {
-        case Some(user) if (crypto.sign(password, user.secretKey.getBytes).equals(user.passwordHash)) =>
-            Authenticated(crypto.encryptAES(s"$userName|$password"))
+        case Some(user) if isValid(user) => Authenticated(crypto.encryptAES(s"$userName|$password"))
         case _ => Unauthenticated
       }
     }
--- test/services/AuthenticationServiceSpec.scala
+++ test/services/AuthenticationServiceSpec.scala
@@ -4,38 +4,55 @@ import org.scalatestplus.play.PlaySpec
 import org.scalatest.mock.MockitoSugar
 import play.api.libs.Crypto
 import java.util.Date
-import org.mockito.Mockito
+import org.mockito.Mockito.when
 import models.user.User
 import scala.async.Async
 import models.auth.Authenticated
 import play.api.test.FutureAwaits
 import org.scalatest.concurrent.Timeouts
 import play.api.test.DefaultAwaitTimeout
+import models.auth.Unauthenticated
 
 class AuthenticationServiceSpec extends PlaySpec with MockitoSugar with FutureAwaits with DefaultAwaitTimeout {
   import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  val generatedToken = "genreated token"
+  val generatedToken = "generated token"
+  val userName = "username"
+  val password = "password"
+  val user = User(userName, "password-secret", "password-hash")
   
   trait testAuthenticationService {
 	  val crypto = mock[Crypto]
     val userService = mock[UserService]
     val service = new CryptoAuthenticationService(crypto, userService)
+	  when(crypto.encryptAES(s"$userName|$password")).thenReturn(generatedToken)
   }
   
-  "test" should {
-    "pass" in new testAuthenticationService {
-      val userName = "username"
-      val password = "password"
-      val user = User("username", "password-secret", "password-hash")
+  "authenticate" should {
+    "give authenticated for valid username and password" in new testAuthenticationService {
+      when(crypto.sign(password, user.secretKey.getBytes)).thenReturn(user.passwordHash)
+      when(userService.forName(userName)).thenReturn(Async.async { Some(user) })
       
-      Mockito.when(crypto.encryptAES(s"$userName|$password")).thenReturn(generatedToken)
-      Mockito.when(crypto.sign(password, user.secretKey.getBytes)).thenReturn(user.passwordHash)
-      Mockito.when(userService.forName(userName)).thenReturn(Async.async { Some(user) })
-      
-      val result = service.authenticate(userName, "password")
+      val result = service.authenticate(userName, password)
       
       await(result) mustBe Authenticated(generatedToken)
+    }
+    
+    "give unauthenticated for invalid username" in new testAuthenticationService {
+      when(userService.forName(userName)).thenReturn(Async.async { None })
+
+      val result = service.authenticate(userName, password)
+
+      await(result) mustBe Unauthenticated
+    }
+
+    "give unauthenticated for invalid password" in new testAuthenticationService {
+      when(crypto.sign(password, user.secretKey.getBytes)).thenReturn("wrong hash")
+      when(userService.forName(userName)).thenReturn(Async.async { Some(user) })
+
+      val result = service.authenticate(userName, password)
+
+      await(result) mustBe Unauthenticated
     }
   }
 }
